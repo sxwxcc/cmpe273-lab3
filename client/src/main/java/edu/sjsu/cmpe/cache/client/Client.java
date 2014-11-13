9eaa1554d7e85d7fea41365c61d338f181e35b87
sxwxcc <sxwxcc@gamil.com>
2014-11-13 12:06:10 +0800

    Update Client.java

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -8,15 +8,18 @@ import com.google.common.hash.Hashing;
 
 public class Client {
 	public static void main(String[] args) throws Exception {
-		
+
 		System.out.println("Starting Cache Client...");
-		
-		CacheServiceInterface cache1 = new DistributedCacheService("http://localhost:3000");
-		CacheServiceInterface cache2 = new DistributedCacheService("http://localhost:3001");
-		CacheServiceInterface cache3 = new DistributedCacheService("http://localhost:3002");
-		
-		List<CacheServiceInterface> servers = Lists.newArrayList(cache1, cache2,
-				cache3);
+
+		CacheServiceInterface cache1 = new DistributedCacheService(
+				"http://localhost:3000");
+		CacheServiceInterface cache2 = new DistributedCacheService(
+				"http://localhost:3001");
+		CacheServiceInterface cache3 = new DistributedCacheService(
+				"http://localhost:3002");
+
+		List<CacheServiceInterface> servers = Lists.newArrayList(cache1,
+				cache2, cache3);
 
 		HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
 		hashmap.put(1, "a");
@@ -29,21 +32,52 @@ public class Client {
 		hashmap.put(8, "h");
 		hashmap.put(9, "i");
 		hashmap.put(10, "j");
-		
-		for (int i = 0; i < hashmap.size() + 1; i++) {
 
-			int bucket = Hashing.consistentHash(Hashing.md5().hashLong(i),servers.size());
-			servers.get(bucket).put(i,hashmap.get(i));
-			System.out.println("put" + i + ":" + hashmap.get(i) + "=>servers.get(bucket)");
-			}
-		
-		for (int i = 0; i < hashmap.size() + 1; i++) {
+		for (int i = 1; i < hashmap.size() + 1; i++) {
 
-			int bucket = Hashing.consistentHash(Hashing.md5().hashLong(i),servers.size());
+			int bucket = Hashing.consistentHash(Hashing.md5().hashLong(i),
+					servers.size());
+			servers.get(bucket).put(i, hashmap.get(i));
+			System.out.println("put " + i + ":" + hashmap.get(i)
+					+ "=>servers.get(" + (bucket + 1) + ")");
+		}
+
+		for (int i = 1; i < hashmap.size() + 1; i++) {
+
+			int bucket = Hashing.consistentHash(Hashing.md5().hashLong(i),
+					servers.size());
 			servers.get(bucket).get(i);
-			System.out.println("get" + i + ":" + hashmap.get(i) + "=>servers.get(bucket)");
-			}
-		
+
+			System.out.println("get" + i + ":" + hashmap.get(i)
+					+ "=>servers.get(" + (bucket+1) + ")");
+		}
+
 		System.out.println("Existing Cache Client...");
 	}
 }
+
+/*
+ * 
+ * package edu.sjsu.cmpe.cache.client;
+ * 
+ * public class Client {
+ * 
+ * public static void main(String[] args) throws Exception {
+ * System.out.println("Starting Cache Client..."); 
+ * CacheServiceInterface cache = new DistributedCacheService( "http://localhost:3000");
+ * 
+ * cache.put(1, "foo"); System.out.println("put(1 => foo)");
+ * 
+ * String value = cache.get(1); System.out.println("get(1) => " + value);
+ * 
+ * System.out.println("Existing Cache Client..."); }
+ * 
+ * }
+ * 
+ * String value = cache.get(1); System.out.println("get(1) => " + value);
+ * 
+ * System.out.println("Existing Cache Client..."); }
+ * 
+ * }
+ */
+
